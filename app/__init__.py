3e6f80bf5083ba920098c216503c441dc2a48992
Evgeniy <serganates@gmail.com>
2024-11-28 23:04:10 +0300

    Authentication has been created

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
+
+# Application
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Login in application
+login = LoginManager(app)
+login.login_view = 'login'
+
+# Database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+from urllib.parse import urlsplit
+from app.models import User
+from flask import request
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'user': 'levigin'}
     posts = [
         {
             'author': {'user': 'Jon'},
@@ -18,13 +23,30 @@ def index():
             'body': "the Avengers movie was so cool"
         }
     ]
-    return render_template("index.html", user=user, posts=posts)
+    return render_template("index.html", title='Home Page', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
-        return redirect(url_for('index'))
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password!")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+            return redirect(next_page)
     return render_template('login.html', title="Sing in", form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
